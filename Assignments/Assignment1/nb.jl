b26570df6a3066b7a0d043002d7ac7c7eb550071
ecacikgoz97 <ecacikgoz97@gmail.com>
2022-02-27 19:34:20 +0300

    Naive Bayes function comments added

--- Assignments/Assignment1/nb.jl
+++ Assignments/Assignment1/nb.jl
@@ -1,6 +1,21 @@
-using Languages, Statistics
+using Statistics
+
 
 function dictFrequency(dictionary, scale)
+    """
+    Calculate the fraction of each word over among all words in that document
+    for corresponding class and take the loglikelihood.
+    
+    Arguments:
+        dictionary(dict): class specific dictionary that contains number of 
+                          words in each document.
+        scale(Float): normalizing factor.
+                        
+    Return:
+        new_dict(dict): dictionary that stores the log-likelihoods for given
+                        document.
+    """
+    
     new_dict = Dict()
     for (key, values) in dictionary
         new_dict[key] = log(values / scale)
@@ -8,7 +23,19 @@ function dictFrequency(dictionary, scale)
     return new_dict
 end
 
+
 function classPriors(data)
+    """
+    Calcualte class priors.
+    *Since we have equal number of positive and negative classes,
+    it may be not used.
+    
+    Arguments:
+        data(list): nested list of the reviews as [[words], class].
+    
+    Return:
+        pririors(array): 2-dimensional array of scores.
+    """
     class1 = 0
     class2 = 0
     for review in data
@@ -21,7 +48,24 @@ function classPriors(data)
     priors=[class1/(class1+class2), class2/(class1+class2)]
 end
 
+
 function prediction(review, WordPos_freq, WordNeg_freq)
+    """
+    This function calculetes the prediction of a review,
+    according to the log-likelihood of the corresponding words.
+    It makes the prediction for each word in all the documents,
+    irrespective of classes, calcuate the likelihood and store it.
+    It assigns the label for grater sum probability.
+    
+    Arguments:
+        review(list): list of words in all classes.
+        WordPos_freq(dict): Word of frequencies for positive class.
+        WordNeg_freq(dict): Word of frequencies for negative class.
+    
+    returns:
+        label(scalar): Prediction of given review as scalar. 1 for
+                       positive reviews 2 for negative reviews.
+    """
     ProbPos, ProbNeg = [], []
     for word in review
         if haskey(WordPos_freq, word)
@@ -43,7 +87,21 @@ function prediction(review, WordPos_freq, WordNeg_freq)
     return label
 end
 
+
 function accuracy(data, WordPos_freq, WordNeg_freq)
+    """
+    Calculate the accuracy for given data according to
+    the outputs of our predictions.
+    
+    Arguments:
+        data(list): Nested list of reviews and labels.
+        WordPos_freq(dict): Word of frequencies for positive class.
+        WordNeg_freq(dict): Word of frequencies for negative class.
+    
+    Return:
+        accuracy(float): accuracy of our naive approach.
+    
+    """
     res = []
     for (review, label) in data
         flag = label==prediction(review, WordPos_freq, WordNeg_freq)
